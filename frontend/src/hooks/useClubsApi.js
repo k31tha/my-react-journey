3836a70a3b93618ca5f17fda9ad82c425c684134
k31tha <keith@kdasoft.co.uk>
2020-03-04 21:25:00 +0000

    changed to use a reduced

--- frontend/src/hooks/useClubsApi.js
+++ frontend/src/hooks/useClubsApi.js
@@ -1,10 +1,18 @@
-import React, {useState,useEffect} from "react";
+import React, {useState,useEffect,useReducer} from "react";
+import clubFetchReducer from "../reducers/clubFetchReducer";
 
 const useClubsApi = (initialClubs) => {
-    const [clubs, setClubs] = useState( initialClubs );
-    const [isLoading, setIsLoading] = useState(false);
-    const [isError, setIsError] = useState(false);
-    useEffect(() => {
+    //const [clubs, setClubs] = useState( initialClubs );
+    //const [isLoading, setIsLoading] = useState(false);
+    //const [isError, setIsError] = useState(false);
+
+    const [state, dispatch] = useReducer(clubFetchReducer, {
+        isLoading: false,
+        isError: false,
+        clubs: initialClubs,
+      });
+
+    /* useEffect(() => {
       const fetchData = async () => {
         setIsError(false);
         setIsLoading(true);
@@ -19,6 +27,23 @@ const useClubsApi = (initialClubs) => {
       fetchData();
     }, []);
     return [{ clubs, isLoading, isError }];
-  }
+  } */
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_INIT' });
+      try {
+        //const result = await axios(url);
+        //dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+        dispatch({ type: 'FETCH_SUCCESS', payload: initialClubs });
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAILURE' });
+      }
+    };
+    fetchData();
+  }, []);
+
+  return [state];
+}
 
   export default useClubsApi;
--- frontend/src/reducers/clubFetchReducer.js
+++ frontend/src/reducers/clubFetchReducer.js
@@ -1,4 +1,19 @@
   
   const clubFetchReducer = (state, action) => {
-    return state;
+    switch (action.type) {
+        case 'FETCH_INIT':
+          return { ...state,isLoading: true,
+            isError: false };
+        case 'FETCH_SUCCESS':
+          return { ...state,isLoading: false,
+            isError: false,
+        clubs: action.payload };
+        case 'FETCH_FAILURE':
+          return { ...state, isLoading: false,
+            isError: true };
+        default:
+          throw new Error();
+      }
   };
+
+  export default clubFetchReducer;
